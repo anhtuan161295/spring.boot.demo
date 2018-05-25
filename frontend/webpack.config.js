3f560167a53cdafab5218034a30a50697f9a60bb
QQ <anhtuan161295@gmail.com>
2018-05-26 00:57:24 +0700

    Working webpack + html loader + maven plugin to execute command

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,32 +1,48 @@
+'use strict';
+
 var path = require('path');
+const glob = require('glob');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
 
 module.exports = {
     entry: './src/App.js',
+    // entry: { main: glob.sync('./src/**/*.js*') },
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: 'bundle.js'
     },
     module: {
         rules: [
+            // {
+            //     test: /\.js$/,
+            //     exclude: /node_modules/,
+            //     loader: ['jshint-loader']
+            // },
             {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: ['babel-loader']
             },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.svg$/,
-                loader: 'svg-inline-loader'
-            }
+            { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+            { test: /\.svg$/, use: ['svg-inline-loader'] },
+            { test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader'] },
+            { test: /\.pug$/, use: ["html-loader", "pug-html-loader"] }
+
+
+
         ]
     },
     resolve: {
         extensions: ['*', '.js', '.jsx']
-    }
-
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './public/index.html',
+            favicon: './public/favicon.ico'
+        })
+    ]
 };
 
 // module.exports = {
